58fc4eccfedf55ca285227bcdf4eb8162620dcfb
Alberto Pumarejo <alberto_pumarejo@hotmail.com>
2019-12-17 12:43:24 -0500

    031

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -12,8 +12,9 @@ class CategoriaController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        if (!$request->ajax()) return redirect('/');
         $categorias = Categoria::all();
         return $categorias;
     }
@@ -26,6 +27,7 @@ class CategoriaController extends Controller
      */
     public function store(Request $request)
     {
+        if (!$request->ajax()) return redirect('/');
         $categoria = new Categoria();
         $categoria->nombre = $request->nombre;
         $categoria->descripcion = $request->descripcion;
@@ -42,6 +44,7 @@ class CategoriaController extends Controller
      */
     public function update(Request $request)
     {
+        if (!$request->ajax()) return redirect('/');
         $categoria = Categoria::findOrFail($request->id);
         $categoria->nombre = $request->nombre;
         $categoria->descripcion = $request->descripcion;
@@ -51,6 +54,7 @@ class CategoriaController extends Controller
 
     public function desactivar(Request $request)
     {
+        if (!$request->ajax()) return redirect('/');
         $categoria = Categoria::findOrFail($request->id);
         $categoria->condicion = '0';
         $categoria->save();
@@ -58,6 +62,7 @@ class CategoriaController extends Controller
 
     public function activar(Request $request)
     {
+        if (!$request->ajax()) return redirect('/');
         $categoria = Categoria::findOrFail($request->id);
         $categoria->condicion = '1';
         $categoria->save();
